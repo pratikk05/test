10a8e562b86d35d9f831b97960c1856718daf004
Pratik Naik <71076420+pratikk05@users.noreply.github.com>
2024-05-17 00:03:13 +0530

    t

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
+
 
 console.log('Welcome to Vite + JS + Webflow!')
 
-createBadge()
-animateTitle()
